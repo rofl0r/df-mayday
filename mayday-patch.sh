40ac87ca84286c94bb20264ebfb4a8aed1bfd4e4
rofl0r <retnyg@gmx.net>
2010-11-12 22:06:25 +0100

    tested to work with 31.17, added some hints what the script is doing

--- mayday-patch.sh
+++ mayday-patch.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
+echo "*** mayday-patchset for POSIX by rofl0r ***"
+echo -ne "\n"
+echo "trying to apply mayday.diff...."
 LOG=mayday-patch.log
 patch -p1 --verbose < mayday.diff > $LOG
 ERRS=`cat $LOG | grep "FAILED"`
 if [[ ! -z "$ERRS" ]]
 then
-	echo "there were some errors, manual intervention is required\n"
+	echo -ne "there were some errors, manual intervention is required\n"
 	echo $ERRS
 	echo "for more details see $LOG"
 else
+	echo "success! now fixing the characters in language files."
+	echo "hold your breath this may take a minute or two...."
 	find ./raw/objects -name 'language*' -exec ./apply-charpatch.sh {} \;
-	echo "success!"
+	echo "done!"
 fi
