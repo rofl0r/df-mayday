8ad8d6bc6c7cbcd04cfcc3e74ee462fbdcdbb608
rofl0r <retnyg@gmx.net>
2010-11-12 19:02:43 -0800

    improved diff-generator's usability

--- utils/makediff.sh
+++ utils/makediff.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 # use this on a backup copy of actual DF and a MAYDAYed Df
 # contents will be trashed !
+THIS=`dirname $0`
 ORG=$1
 MAYDAY=$2
-./clean.sh $ORG
-./clean.sh $MAYDAY
+$THIS/clean.sh $ORG
+$THIS/clean.sh $MAYDAY
 diff -r -C1 --exclude=language* -w -B -E  $ORG $MAYDAY > mayday.diff
