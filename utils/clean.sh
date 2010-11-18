a7c4f2d27d2f831496900e96752cfad351909736
rofl0r <retnyg@gmx.net>
2010-11-18 22:34:36 +0100

    made clean.sh foolproof since it is deleting stuff

--- utils/clean.sh
+++ utils/clean.sh
@@ -1,19 +1,31 @@
-#/usr/bin/env bash
+#!/usr/bin/env bash
 # parameter: directory with DF or DFG
 # purpose: rip off everything unneeded for diffing.
-if [[ -z "$1" ]]
+DFDIR=$1
+ROOTDIR="/"
+DOTDIR="."
+if [[ -z "$DFDIR" ]]
 then
 	echo "supply a valid path as first parameter!"
+elif [[ "$DFDIR" == "$ROOTDIR" ]]
+then
+	echo "omg. you REALLY shouldnt apply this on your root dir."
+elif [[ "$DFDIR" == "$DOTDIR" ]]
+then
+	echo "you have to cd out of the distrib dir. just cd .. and call again"
+elif [[ ! -e "$DFDIR/data/init" ]]
+then
+	echo "your target doesnt look like a DF dir. exitting."
 else
 	MDTMP=/tmp/mdtmp
 	mkdir $MDTMP
-	mv $1/data/init $MDTMP/init
-	mv $1/raw/objects $MDTMP/objects
-	rm -rf $1
-	mkdir $1
-	mkdir $1/data
-	mv $MDTMP/init $1/data/
-	mkdir $1/raw
-	mv $MDTMP/objects $1/raw/
+	mv $DFDIR/data/init $MDTMP/init
+	mv $DFDIR/raw/objects $MDTMP/objects
+	rm -rf $DFDIR
+	mkdir $DFDIR
+	mkdir $DFDIR/data
+	mv $MDTMP/init $DFDIR/data/
+	mkdir $DFDIR/raw
+	mv $MDTMP/objects $DFDIR/raw/
 	rm -rf $MDTMP
 fi
