be1a145e3458e106bb62835195858712030b3d37
rofl0r <retnyg@gmx.net>
2012-02-20 18:14:09 +0100

    make makediff executable and more friendly

--- utils/makediff.sh
+++ utils/makediff.sh
@@ -4,6 +4,10 @@
 THIS=`dirname $0`
 ORG=$1
 MAYDAY=$2
+if [ -z "$ORG" ] || [ -z "$MAYDAY" ] ; then
+	echo syntax: $0 df_path/ dfg_path/
+	exit 1
+fi
 $THIS/clean.sh $ORG
 $THIS/clean.sh $MAYDAY
 diff -r -C1 --exclude=language* -w -B -E  $ORG $MAYDAY > mayday.diff
