09ac847e1d93e2972ab17520e4256995541c5283
rofl0r <retnyg@gmx.net>
2013-04-16 13:36:17 +0200

    get_debian_32bit_libs.sh: check for availability of utils and abort on error

--- get_debian_32bit_libs.sh
+++ get_debian_32bit_libs.sh
@@ -1,18 +1,29 @@
 #!/bin/sh
 # Get 32-bit libs required for Dwarf Fortress on 64-bit Linux.
+# requires debian's dpkg program.
 
-if [ ! -d temp ]
-then 
-  mkdir temp
-fi
+check_program() {
+	if ! which "$1" >/dev/null 2>&1 ; then
+		echo "required program $1 not installed. aborting."
+		exit 1
+	fi
+}
+
+check_program wget
+check_program dpkg
+
+mkdir -p temp
 
 # Save the file from $1 as $2 using wget.
 fetchextract() {
-url="$1"
-file="$2"
-wget -O "temp/$file" "$url"/"$file" || die
-dpkg -x "temp/$file" temp
+	url="$1"
+	file="$2"
+	wget -O "temp/$file" "$url"/"$file"
+	dpkg -x "temp/$file" temp
 }
+
+# abort on error
+set -e
 
 # Get the required libs:
 fetchextract "http://ftp.us.debian.org/debian/pool/main/s/sdl-image1.2" "libsdl-image1.2_1.2.10-2+b2_i386.deb"
@@ -20,9 +31,11 @@ fetchextract "http://ftp.us.debian.org/debian/pool/main/s/sdl-ttf2.0" "libsdl-tt
 fetchextract "http://ftp.us.debian.org/debian/pool/main/o/openal-soft" "libopenal1_1.12.854-2_i386.deb"
 
 # Put them to df's libs directory:
-cp temp/usr/lib/libopenal.so.1 libs/libopenal.so
-cp temp/usr/lib/libSDL_image-1.2.so.0 libs/.
-cp temp/usr/lib/libSDL_ttf-2.0.so.0 libs/.
+mkdir -p libs
+
+mv temp/usr/lib/libopenal.so.1.12.854 libs/libopenal.so
+mv temp/usr/lib/libSDL_image-1.2.so.0.8.2 libs/libSDL_image-1.2.so.0
+mv temp/usr/lib/libSDL_ttf-2.0.so.0.6.3 libs/libSDL_ttf-2.0.so.0
 
 # Clean up
 rm -rf temp
